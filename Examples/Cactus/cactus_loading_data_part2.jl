a2ddb97b76ae7675df85bdfa6af5278d044418ab
Samyak S Sarnayak <samyak201@gmail.com>
2020-01-29 23:28:28 +0530

    Complete cactus example

--- Examples/Cactus/cactus_loading_data_part2.jl
+++ Examples/Cactus/cactus_loading_data_part2.jl
@@ -1,7 +1,7 @@
 using CSV
 using DataFrames
 using JLD
-
+cd("Examples/Cactus")
 input_file = "train.csv"
 df = CSV.read(input_file)
 
@@ -28,6 +28,10 @@ end
 
 test_X = reshape_image_array(train_data["test_X_orig"])
 train_X = reshape_image_array(train_data["train_X_orig"])
+train_Y = reshape(train_Y, (1, size(train_Y)[1]))
+# test_Y = reshape(test_Y, (1, size(test_Y)[1]))
 
 include("../../NeuralNetwork.jl")
-neural_network_dense(train_X, train_Y, [3072, 1, 1], 0.01)
+# neural_network_dense(train_X, train_Y, [3072, 1, 1], 0.05)
+parameters, activations = neural_network_dense(train_X, train_Y, [3072, 10, 10, 1], 100, 0.1)
+predicts, accuracy = predict(train_X, train_Y, parameters, activations)
